fa723051ac248d4509d547e6b55937b6120a9767
sasaplus1 <sasaplus1@gmail.com>
2023-11-11 00:21:54 +0900

    add test

--- index.test.ts
+++ index.test.ts
@@ -9,11 +9,21 @@ describe('isEqual', function() {
   it('test with fast-check', function() {
     fc.assert(
       fc.property(fc.anything({ withDate: true }), function(a) {
+        return isEqual(a, a) === _.isEqual(a, a);
+      })
+    );
+  });
+  it('return true if primitives are passed', function() {
+    // TODO: without Array, add more other primitives
+    const arb = fc.clone(fc.oneof(fc.integer(), fc.string(), fc.array(fc.integer())), 2);
+
+    fc.assert(
+      fc.property(arb, function([a, b]) {
         if (!isCi) {
-          console.log(a);
+          console.log(a, b);
         }
 
-        return isEqual(a, a) === _.isEqual(a, a);
+        return isEqual(a, b);
       })
     );
   });
